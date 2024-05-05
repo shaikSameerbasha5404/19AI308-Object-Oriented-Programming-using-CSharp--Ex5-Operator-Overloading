845ca9fe4b61077f7dc4d6bb8b4e0c2020f322e7
SHAIK SAMEER BASHA <118707756+shaikSameerbasha5404@users.noreply.github.com>
2024-05-05 10:30:06 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace OperatorOverloading
+class MyClass
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    private int value;
 
-        }
+    // Default constructor
+    public MyClass()
+    {
+        this.value = 0;
+    }
+
+    // Parameterized constructor
+    public MyClass(int value)
+    {
+        this.value = value;
+    }
+
+    // Getter for value
+    public int GetValue()
+    {
+        return value;
+    }
+
+    // Overloading equality operator
+    public static bool operator ==(MyClass obj1, MyClass obj2)
+    {
+
+        // Check if either of the objects is null
+        if (obj1 is null || obj2 is null)
+            return false;
+
+        // Compare the values of the objects
+        return obj1.value == obj2.value;
+    }
+
+    // Overloading inequality operator
+    public static bool operator !=(MyClass obj1, MyClass obj2)
+    {
+        return !(obj1 == obj2);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass obj1 = new MyClass(10);
+        MyClass obj2 = new MyClass(); // Using default constructor
+        MyClass obj3 = new MyClass(10);
+
+        // Check equality
+        Console.WriteLine("Are obj1 and obj2 equal? " + (obj1 == obj2));
+        Console.WriteLine("Are obj1 and obj3 equal? " + (obj1 == obj3));
     }
 }
